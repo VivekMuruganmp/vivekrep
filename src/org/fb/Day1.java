e63df6e71fd49391c8937d879e041c011359036b
VivekMuruganmp <vivekmechanical307@gmail.com>
2023-02-12 20:21:19 +0530

    my dute

---
+++ src/org/fb/Day1.java
@@ -0,0 +1,17 @@
+package org.fb;
+
+public class Day1 {
+	
+private void add(int a, int b) {
+	int c = a+b;
+	System.out.println(c);
+
+}
+
+	public static void main(String[] args) {
+		Day1 day = new Day1();
+		day.add(10, 30);
+
+	}
+
+}
